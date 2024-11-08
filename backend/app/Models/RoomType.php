5099edb4b1ec665ac62ddc4bf3aaf7ad327f1aed
LamsalApil5 <lamsalapil5@gmail.com>
2024-11-08 20:15:41 +0545

    room type table

---
+++ backend/app/Models/RoomType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class RoomType extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name', 
+        'image',  
+        'description', 
+        'price', 
+        'slug'
+    ];
+
+    protected $table = 'RoomTypes'; 
+
+    public function scopeFilter($query, array $filters)
+    {
+        if ($filters['search'] ?? false) {
+            $query->where('name', 'like', '%' . $filters['search'] . '%')
+                ->orWhere('image', 'like', '%' . $filters['search'] . '%');
+        }
+    }
+}
