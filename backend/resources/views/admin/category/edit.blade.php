f05a513674a28ccdea1a3c3f709f34be6a058758
LamsalApil5 <lamsalapil5@gmail.com>
2024-11-10 19:53:03 +0545

    edit

--- backend/resources/views/admin/category/edit.blade.php
+++ backend/resources/views/admin/category/edit.blade.php
@@ -5,35 +5,34 @@
                 <div class="col-12">
                     <div class="card">
                         <div class="card-header d-flex justify-content-between">
-                            <div class="header-title">
-                                <h4 class="card-title">Category</h4>
-                            </div>
+                            <h4 class="card-title">Edit Category</h4>
                             <div class="back">
-                                <a href="{{route('categories')}}" class=" text-center btn btn-primary btn-icon mt-lg-0 mt-md-0 mt-3">
+                                <a href="{{ route('categories') }}" class="btn btn-primary btn-icon mt-lg-0 mt-md-0 mt-3">
                                     <i class="btn-inner">
-                                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" viewBox="0 0 25 25" fill="none" stroke="currentColor"><path  d="M24 12.001H2.914l5.294-5.295-.707-.707L1 12.501l6.5 6.5.707-.707-5.293-5.293H24v-1z" data-name="Left"/></svg>
+                                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" viewBox="0 0 25 25" fill="none" stroke="currentColor">
+                                            <path d="M24 12.001H2.914l5.294-5.295-.707-.707L1 12.501l6.5 6.5.707-.707-5.293-5.293H24v-1z" data-name="Left" />
+                                        </svg>
                                     </i>
                                 </a>
                             </div>
                         </div>
                         <div class="card-body mt-2">
-                            <form  method="POST" action="{{ route('category.update',$category->slug) }}">
+                            <form method="POST" action="{{ route('category.update', $category->slug) }}">
                                 @csrf
                                 @method('PUT')
-                                <div class="form-outline mb-3">
-                                    <label class="form-label" for="name">Name</label>
-                                    <input type="text" name="name" id="name" class="form-control @error('name') is-invalid @enderror {{ $errors->has('name') ? 'error' : '' }}"
-                                        value="{{ $category->name }}" />
+                                <div class="form-outline mb-4">
+                                    <label class="form-label" for="name">Category Name</label>
+                                    <input type="text" name="name" id="name" class="form-control @error('name') is-invalid @enderror" value="{{ old('name', $category->name) }}" required />
                                     @error('name')
-                                    <span class="text-danger">{{$message}}</span>
+                                    <span class="text-danger">{{ $message }}</span>
                                     @enderror
                                 </div>
-                               
-                                <button type="submit" class="btn btn-primary btn-block rounded-pill mb-4">Update</button>
+                                <button type="submit" class="btn btn-success btn-block rounded-pill mb-3">Update</button>
                             </form>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
+    </div>
 </x-app-layout>
