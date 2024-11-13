743dfb3cb5443192367c23a79c40c26a2f676b1f
LamsalApil5 <lamsalapil5@gmail.com>
2024-11-13 10:26:37 +0545

    error room type fix

--- backend/database/migrations/2024_11_11_034442_create_room_types_table.php
+++ backend/database/migrations/2024_11_11_034442_create_room_types_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('room_types', function (Blueprint $table) {
+        Schema::create('roomtypes', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->string('image')->nullable();
